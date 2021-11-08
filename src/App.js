71c7d7e42d4278a9deba2b744b93197dbcf34488
Supachai Chanarittichai <supachaic@outlook.com>
2021-11-08 20:09:37 +0900

    Cat memes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,248 @@
+/*
+ * We are going to be using the useEffect hook!
+ */
+import { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import { Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
+import {
+  Program, Provider, web3
+} from '@project-serum/anchor';
 
-// Constants
+import idl from './idl.json';
+import kp from './keypair.json'
+
+// Change this up to be your Twitter if you want.
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+// const TEST_GIFS = [
+// 	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+// 	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+// 	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+// 	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+// ]
+
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram } = web3;
+
+// Create a keypair for the account that will hold the GIF data.
+// let baseAccount = Keypair.generate();
+
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+
+// Get our program's id form the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+
+// Set our network to devent.
+const network = clusterApiUrl('devnet');
+
+// Control's how we want to acknowledge when a trasnaction is "done".
+const opts = {
+  preflightCommitment: "processed"
+}
+
+
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [memeList, setMemeList] = useState([]);
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+            /*
+          * The solana object gives us a function that will allow us to connect
+          * directly with the user's wallet!
+          */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+            );
+          
+          /*
+           * Set the user's publicKey in state to be used later!
+           */
+          setWalletAddress(response.publicKey.toString());
+        
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const createMemeAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping")
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getMemeList();
+  
+    } catch(error) {
+      console.log("Error creating BaseAccount account:", error)
+    }
+  }
+
+  const sendMeme = async () => {
+    if (inputValue.length === 0) {
+      console.log("No meme link given!")
+      return
+    }
+    console.log('Meme link:', inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+  
+      await program.rpc.addMeme(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+        },
+      });
+      console.log("MEME sucesfully sent to program", inputValue)
+  
+      await getMemeList();
+      setInputValue('');
+    } catch (error) {
+      console.log("Error sending MEME:", error)
+    }
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => {
+    // If we hit this, it means the program account hasn't be initialized.
+    if (memeList === null) {
+      return (
+        <div className="connected-container">
+          <button className="cta-button submit-gif-button" onClick={createMemeAccount}>
+            Do One-Time Initialization For MEME Program Account
+          </button>
+        </div>
+      )
+    } else {
+      return (<div className="connected-container">
+        <input 
+          type="text" 
+          placeholder="Enter cat meme link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button className="cta-button submit-gif-button" onClick={sendMeme}>Submit</button>
+        <div className="gif-grid">
+          {memeList.map((item, index) => (
+            <div className="gif-item" key={index}>
+              <img src={item.memeLink} alt="Cat Meme link"/>
+            </div>
+          ))}
+        </div>
+      </div>)
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    window.addEventListener('load', async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
+  const getMemeList = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      
+      console.log("Got the account", account)
+      setMemeList(account.memeList)
+  
+    } catch (error) {
+      console.log("Error in getGifs: ", error)
+      setMemeList(null);
+    }
+  }
+  
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching Meme list...');
+      getMemeList();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [walletAddress]);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🐈 Cat Memes Portal</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your cat memes collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
